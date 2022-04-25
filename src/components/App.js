45cf4b8cd5a9f750eec838cc484e6002fb45a462
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-25 07:56:33 -0500

    Remove routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
 
 const App = () => {
   return (
-    <HashRouter>
+    <div className="app">
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
+      <Home />
       <Footer />
-    </HashRouter>
+    </div>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
-import { Link } from 'react-router-dom';
-
 const Header = () => {
     return (
         <header>
             <h1 className="title">Weiminh Lam</h1>
             <ul className="nav">
-                <li><Link to="#home">Home</Link></li>
-                <li><Link to="#about">About</Link></li>
-                <li><Link to="#projects">Projects</Link></li>
-                <li><Link to="skills">Skills</Link></li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#skills">Skills</a></li>
             </ul>
         </header>
     )
