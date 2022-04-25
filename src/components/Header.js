76d030ca678575afa6a56fae8d3c0384dff0827f
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-25 08:36:58 -0500

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import avatar from '../assets/avatar.png';
+
 const Header = () => {
     return (
         <header>
-            <h1 className="title">Weiminh Lam</h1>
+            <a className="header-left" href="#home">
+                <img className="avatar" src={avatar} alt="Weiminh Lam"></img>
+                <h1 className="title">Weiminh Lam</h1>
+            </a>
             <ul className="nav">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
