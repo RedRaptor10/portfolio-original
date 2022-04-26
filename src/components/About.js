efb4251e2e7359de405b46457e16e1af6f2d5298
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-26 06:46:43 -0500

    Add About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,58 @@
+import avatar from '../assets/avatar.png';
+
 const About = () => {
     return (
         <section id="about">
-            About
+            <div className="section-top">
+                <div className="section-left">
+                    <h1>About</h1>
+                    <p>
+                        Hi! My name is Weiminh Lam. I am a self-taught <b>full-stack web developer</b> with a BS degree in Computer Science.
+                        I have been learning web development since the beginning of 2021 through online resources such as&nbsp;
+                        <a href="https://www.theodinproject.com" target="_blank" rel="noreferrer">The Odin Project</a>. I am constantly
+                        learning and improving my skills in order to become a proficient web developer.
+                    </p>
+                </div>
+                <div className="section-right">
+                    <img className="avatar" src={avatar} alt="Weiminh Lam"></img>
+                </div>
+            </div>
+            <div className="section-bottom">
+                <div className="section-left">
+                    <p>
+                        I have always found it fun to build websites and showcase them to the world. I've actually been creating websites since
+                        I was young, using services like <em>Yahoo! GeoCities</em> and <em>WordPress</em>. In fact, I currently run a&nbsp;
+                        <a href="https://www.kombatakademy.com" target="_blank" rel="noreferrer">WordPress</a> site that educates people on how
+                        to play fighting games! Aside from coding, I enjoy playing video games, watching anime, and creating&nbsp;
+                        <a href="https://www.youtube.com/RedRaptor10" target="_blank" rel="noreferrer">YouTube</a> videos.
+                    </p>
+                </div>
+                <div className="section-right">
+                    <div id="contact">
+                        <table>
+                            <tr>
+                                <th colspan="2">Contact Information</th>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td><a href="mailto:weiminh_lam@yahoo.com">weiminh_lam@yahoo.com</a></td>
+                            </tr>
+                            <tr>
+                                <td>GitHub</td>
+                                <td><a href="https://www.github.com/RedRaptor10" target="_blank" rel="noreferrer">github.com/RedRaptor10</a></td>
+                            </tr>
+                            <tr>
+                                <td>LinkedIn</td>
+                                <td>
+                                    <a href="https://www.linkedin.com/in/weiminh-lam/339198129" target="_blank" rel="noreferrer">
+                                        linkedin.com/in/weiminh...
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 };
