b75caeb2c046d0b07c469e2670d7a0b4b3f2bb8c
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-27 14:41:20 -0500

    Add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faTools, faFolder } from '@fortawesome/free-solid-svg-icons';
 import avatar from '../assets/avatar.png';
 
 const Header = () => {
@@ -8,10 +10,10 @@ const Header = () => {
                 <h1 className="title">Weiminh Lam</h1>
             </a>
             <ul className="nav">
-                <li><a href="/#">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#skills">Skills</a></li>
-                <li><a href="/#projects">Projects</a></li>
+                <li><a href="/#"><FontAwesomeIcon icon={faHome} />Home</a></li>
+                <li><a href="/#about"><FontAwesomeIcon icon={faUser} />About</a></li>
+                <li><a href="/#skills"><FontAwesomeIcon icon={faTools} />Skills</a></li>
+                <li><a href="/#projects"><FontAwesomeIcon icon={faFolder} />Projects</a></li>
             </ul>
         </header>
     )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+// Add Font Awesome Brand Icons
+library.add(fab);
+
+// Import Additional Icons From Assets Using Webpack
+const icons = {};
+
+function importAll(r) {
+    return r.keys().forEach((key) => (icons[key] = r(key)));
+}
+
+importAll(require.context('../assets/icons', false, /\.(png|jpe?g|svg)$/)); // require.context(directory, (useSubdirectories = true), (regExp));
+
 const Skills = () => {
     return (
         <section id="skills">
@@ -6,33 +22,33 @@ const Skills = () => {
                 <div className="card skills-group">
                     <h2 className="card-title">Front-end</h2>
                     <div className="skills-group-items">
-                        <div>JavaScript</div>
-                        <div>React</div>
-                        <div>HTML5</div>
-                        <div>Pug</div>
-                        <div>CSS3</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-js-square" />JavaScript</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-react" />React</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-html5" />HTML5</div>
+                        <div><img className="skills-icon" src={icons['./pug.svg']} alt="Pug"></img>Pug</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-css3" />CSS3</div>
                     </div>
                 </div>
                 <div className="card skills-group">
                     <h2 className="card-title">Back-end</h2>
                     <div className="skills-group-items">
-                        <div>JavaScript</div>
-                        <div>NodeJS</div>
-                        <div>Express</div>
-                        <div>MongoDB</div>
-                        <div>Firebase</div>
-                        <div>Jest</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-js-square" />JavaScript</div>
+                        <div><img className="skills-icon" src={icons['./jest.svg']} alt="Jest"></img>Jest</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-node-js" />NodeJS</div>
+                        <div><img className="skills-icon" src={icons['./expressjs.svg']} alt="Express"></img>Express</div>
+                        <div><img className="skills-icon" src={icons['./mongodb.svg']} alt="MongoDB"></img>MongoDB</div>
+                        <div><img className="skills-icon" src={icons['./firebase.svg']} alt="Firebase"></img>Firebase</div>
                     </div>
                 </div>
                 <div className="card skills-group">
                     <h2 className="card-title">Miscellaneous</h2>
                     <div className="skills-group-items">
-                        <div>Git</div>
-                        <div>npm</div>
-                        <div>Webpack</div>
-                        <div>GitHub</div>
-                        <div>Heroku</div>
-                        <div>WordPress</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-git" />Git</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-npm" />npm</div>
+                        <div><img className="skills-icon" src={icons['./webpack.svg']} alt="Webpack"></img>Webpack</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-github" />GitHub</div>
+                        <div><img className="skills-icon" src={icons['./heroku.svg']} alt="Heroku"></img>Heroku</div>
+                        <div><FontAwesomeIcon icon="fa-brands fa-wordpress" />WordPress</div>
                     </div>
                 </div>
             </div>
