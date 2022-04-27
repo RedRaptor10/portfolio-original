321ce71fa23bc0cc0547a0f640126379e4366d02
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-27 11:37:36 -0500

    Add Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,41 @@
 const Skills = () => {
     return (
         <section id="skills">
-            Skills
+            <h1>Skills</h1>
+            <div className="skills-wrapper">
+                <div className="card skills-group">
+                    <h2 className="card-title">Front-end</h2>
+                    <div className="skills-group-items">
+                        <div>JavaScript</div>
+                        <div>React</div>
+                        <div>HTML5</div>
+                        <div>Pug</div>
+                        <div>CSS3</div>
+                    </div>
+                </div>
+                <div className="card skills-group">
+                    <h2 className="card-title">Back-end</h2>
+                    <div className="skills-group-items">
+                        <div>JavaScript</div>
+                        <div>NodeJS</div>
+                        <div>Express</div>
+                        <div>MongoDB</div>
+                        <div>Firebase</div>
+                        <div>Jest</div>
+                    </div>
+                </div>
+                <div className="card skills-group">
+                    <h2 className="card-title">Miscellaneous</h2>
+                    <div className="skills-group-items">
+                        <div>Git</div>
+                        <div>npm</div>
+                        <div>Webpack</div>
+                        <div>GitHub</div>
+                        <div>Heroku</div>
+                        <div>WordPress</div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 };
