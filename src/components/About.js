47cea2ba32ac4143467fbab1110c4b794c41db57
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-27 15:52:51 -0500

    Add contact icons

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import avatar from '../assets/avatar.png';
 
 const About = () => {
@@ -32,15 +35,15 @@ const About = () => {
                         <h2 className="card-title">Contact Information</h2>
                         <div className="contact-info">
                             <div className="contact-item">
-                                <div>Email</div>
+                                <div><FontAwesomeIcon icon={faEnvelope} />Email</div>
                                 <div><a href="mailto:weiminh_lam@yahoo.com">weiminh_lam@yahoo.com</a></div>
                             </div>
                             <div className="contact-item">
-                                <div>GitHub</div>
+                                <div><FontAwesomeIcon icon={faGithub} />GitHub</div>
                                 <div><a href="https://www.github.com/RedRaptor10" target="_blank" rel="noreferrer">github.com/RedRaptor10</a></div>
                             </div>
                             <div className="contact-item">
-                                <div>LinkedIn</div>
+                                <div><FontAwesomeIcon icon={faLinkedin} />LinkedIn</div>
                                 <div>
                                     <a href="https://www.linkedin.com/in/weiminh-lam/339198129" target="_blank" rel="noreferrer">
                                         linkedin.com/in/weiminh...
