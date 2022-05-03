425d615da7153ce6fc0a91c8a0e1c540a65a4d79
RedRaptor10 <weiminh_lam@yahoo.com>
2022-05-03 09:08:50 -0500

    Add Projects List

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,14 +40,14 @@ const Projects = () => {
 
     // Clone first and last slides and add them as prev and next slides
     useEffect(() => {
-        const slidesContainer = document.querySelector('.projects-list');
+        const slider = document.querySelector('.projects-slider');
         const slides = document.querySelectorAll('.project');
 
         const firstClone = slides[0].cloneNode(true);
         const lastClone = slides[slides.length - 1].cloneNode(true);
 
-        slidesContainer.append(firstClone);
-        slidesContainer.prepend(lastClone);
+        slider.append(firstClone);
+        slider.prepend(lastClone);
 
         // Set default slide positions
         const newSlides = document.querySelectorAll('.project');
@@ -58,7 +58,7 @@ const Projects = () => {
 
     function shiftSlide(newIndex) {
         const slides = document.querySelectorAll('.project');
-        const btns = document.querySelectorAll('.projects-list-btns > button');
+        const btns = document.querySelectorAll('.projects-slider-btns > button');
         const prev = 0;
         const first = 1;
         const last = slides.length - 2;
@@ -105,38 +105,65 @@ const Projects = () => {
     return (
         <section id="projects">
             <h1>Projects</h1>
-            <div className="projects-list">
-                {projects.map((project, i) => {
-                    return (
-                        <div key={i} className="card project">
-                            <h2 className="card-title">{project.name}<span>{i + 1 + ' / ' + projects.length}</span></h2>
-                            <div className="project-content">
-                                <img src={thumbs['./' + project.thumb]} alt={project.name}></img>
-                                <div className="project-info">
-                                    <div className="project-description">
-                                        {project.description}
-                                    </div>
-                                    <div className="project-tools">
-                                    <FontAwesomeIcon icon={faTools} /><b>Tools: </b>{project.tools}
-                                    </div>
-                                    <div className="project-btns">
-                                        <a href={project.demo} target="_blank" rel="noreferrer">
-                                            <button><FontAwesomeIcon icon={faEye} />View Demo</button>
-                                        </a>
-                                        <a href={project.source} target="_blank" rel="noreferrer">
-                                            <button><FontAwesomeIcon icon={faCode} />Source Code</button>
-                                        </a>
+            <div className="projects-wrapper">
+                <div className="projects-list card">
+                    <h2 className="card-title">All Projects</h2>
+                    <div className="projects-list-table-container">
+                        <table>
+                            <tbody>
+                                {projects.map((project, i) => {
+                                    return (
+                                        <tr key={i}>
+                                            <td onClick={() => {
+                                                pause();
+                                                shiftSlide(i + 1);
+                                            }}><span>{i + 1}</span> ‒ {project.name}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="projects-slider">
+                    {projects.map((project, i) => {
+                        return (
+                            <div key={i} className="card project">
+                                <h2 className="card-title">{project.name}<span>{i + 1 + ' / ' + projects.length}</span></h2>
+                                <div className="project-content">
+                                    <img src={thumbs['./' + project.thumb]} alt={project.name}></img>
+                                    <div className="project-info">
+                                        <div className="project-description">
+                                            {project.description}
+                                        </div>
+                                        <div className="project-tools">
+                                        <FontAwesomeIcon icon={faTools} /><b>Tools: </b>{project.tools}
+                                        </div>
+                                        <div className="project-btns">
+                                            <a href={project.demo} target="_blank" rel="noreferrer">
+                                                <button><FontAwesomeIcon icon={faEye} />View Demo</button>
+                                            </a>
+                                            <a href={project.source} target="_blank" rel="noreferrer">
+                                                <button><FontAwesomeIcon icon={faCode} />Source Code</button>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-            <div className="projects-list-btns">
-                <button className="prev-btn" onClick={() => { pause(); shiftSlide(index - 1) }}>Prev</button>
+            <div className="projects-slider-btns">
+                <button className="prev-btn" onClick={() => {
+                    pause();
+                    shiftSlide(index - 1);
+                }}>Prev</button>
                 <button className="pause-btn" onClick={() => paused ? play() : pause() }><FontAwesomeIcon icon={faPause} /></button>
-                <button className="next-btn" onClick={() => { pause(); shiftSlide(index + 1) }}>Next</button>
+                <button className="next-btn" onClick={() => {
+                    pause();
+                    shiftSlide(index + 1);
+                }}>Next</button>
             </div>
         </section>
     )
