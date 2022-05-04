b612ba33732e2596a970f478557cd9c9509de4ee
RedRaptor10 <weiminh_lam@yahoo.com>
2022-05-04 12:00:25 -0500

    Update projects data

---
+++ src/data/projects.js
@@ -0,0 +1,70 @@
+const projects = [
+    {
+        name: 'Odinbook',
+        description: 'A clone of popular social media website, Facebook. ' +
+            'Users can create an account and log in, view timelines, create posts and comments, ' +
+            'send friend requests, upload a profile picture, and set profile and post visibility. ' +
+            'To get started, either create an account or log in with an existing account:\n' +
+            'Email: johndoe@johndoe.com\nPassword: johndoe',
+        tools: 'NodeJS / Express, Mongoose (MongoDB), JavaScript, React, HTML, CSS',
+        thumb: 'odinbook.jpg',
+        demo: 'https://redraptor10.github.io/odinbook/',
+        source: 'https://github.com/RedRaptor10/odinbook'
+    },
+    {
+        name: 'Fakeddit',
+        description: 'A clone of popular social media website, Reddit. Users can create an account ' +
+            'and log in, browse different subfakeddits, create/delete posts and comments, upvote/downvote ' +
+            'posts and comments, collapse comments, sort posts and comments, filter posts by flair, and ' +
+            'search posts.',
+        tools: 'React, Firebase, JavaScript, HTML, CSS',
+        thumb: 'fakeddit.jpg',
+        demo: 'https://redraptor10.github.io/fakeddit',
+        source: 'https://github.com/RedRaptor10/fakeddit'
+    },
+    {
+        name: 'Game Inventory',
+        description: 'An inventory app that allows a user to manage a game library. ' +
+            'The inventory follows basic CRUD guidelines, allowing the user to Create, ' +
+            'Read, Update, and Delete game data in the database.',
+        tools: 'NodeJS / Express, Mongoose (MongoDB), JavaScript, HTML, CSS',
+        thumb: 'game-inventory.jpg',
+        demo: 'https://glacial-tor-43943.herokuapp.com/',
+        source: 'https://github.com/RedRaptor10/inventory-app'
+    },
+    {
+        name: "Where's Waldo?",
+        description: "A game of Where's Waldo. Players must find characters Waldo, Odlaw, " +
+            "and Wizard by clicking anywhere on the image and selecting the character's name. " +
+            "If the player's time is within the top 10 highest scores, they can submit it to the " +
+            "leaderboard.",
+        tools: 'React, Firebase, JavaScript, HTML, CSS',
+        thumb: 'wheres-waldo.jpg',
+        demo: 'https://redraptor10.github.io/Wheres-Waldo',
+        source: 'https://github.com/RedRaptor10/Wheres-Waldo'
+    },
+    {
+        name: 'Battleship',
+        description: 'A game of Battleship played against an AI. The board can be set up at the ' +
+        'start of the game by dragging each ship to a designated area. Ships can be rotated or ' +
+        'placed automatically. Clicking on an enemy board space will select the area for attack. ' +
+        'The player that destroys all of their enemy ships first wins.',
+        tools: 'JavaScript, HTML, CSS',
+        thumb: 'battleship.jpg',
+        demo: 'https://redraptor10.github.io/Battleship',
+        source: 'https://github.com/RedRaptor10/Battleship'
+    },
+    {
+        name: 'Calculator',
+        description: 'A simple calculator app that features all the basic functions of a calculator. ' +
+            'Users can add, subtract, multiply, and divide numbers. Additionally, users can delete ' +
+            'previously inputted numbers and clear/reset the display. The calculator is able to handle ' +
+            'decimal numbers as well as keyboard inputs.',
+        tools: 'JavaScript, HTML, CSS',
+        thumb: 'calculator.jpg',
+        demo: 'https://redraptor10.github.io/Calculator',
+        source: 'https://github.com/RedRaptor10/Calculator'
+    },
+];
+
+export default projects;
